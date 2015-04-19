cdb6028e75fd7dbb40273d7949f22dd8a558fb97
Kian Ho <hui.kian.ho@googlemail.com>
2015-04-20 00:00:32 +1000

    Added code to run basic ETL into an sqlite db.

--- bin/etl.py
+++ bin/etl.py
@@ -6,11 +6,13 @@ Description:
     ...
 
 Usage:
-    etl.py check <bx-users-csv>
+    etl.py etl <bx-users-csv> <bx-books-csv> <bx-book-ratings-csv> --db DB
+    etl.py check <bx-users-csv> <bx-books-csv> <bx-book-ratings-csv>
 
 Options:
-    check
-
+    etl         Run ETL on the BX data and load into an sqlite db.
+    check       Log the rows in the BX data that look mangled/unusual.
+    --db DB     The sqlite db in which to load the BX data. 
 
 """
 
@@ -18,13 +20,14 @@ import os
 import sys
 import re
 import sqlite3
-import textwrap
-import tabulate
+import json
 import petl as etl
 
-from csv import QUOTE_MINIMAL
+from pprint import pprint
+from csv import QUOTE_ALL
 from collections import OrderedDict, defaultdict
 from docopt import docopt
+from cerberus import Validator
 from unidecode import unidecode # transliterate unicode to ascii
 
 PWD = os.path.dirname(os.path.abspath(__file__))
@@ -33,89 +36,246 @@ PWD = os.path.dirname(os.path.abspath(__file__))
 #   $ uchardet <FILE>
 ENCODING = "windows-1252"
 DELIMITER = ";"
+ESCAPECHAR = "\\"
 
 ERROR_VALUE = None
-WS_PAT = re.compile(r"\s+")
+NULL_VALUE = None
 
 # Expected field patterns.
-ISBN_PAT = re.compile(r"^((\d{12}[\dA-Za-z])|(\d{9}[\dA-Za-z]))$")
 USER_ID_PAT = re.compile(r"^(\d+)$")
 AGE_PAT = re.compile(r"^(\d+)$")
+YEAR_PAT = re.compile(r"^(\d{4})$") # TODO: check for improbably old/new books.
+URL_PAT = re.compile(r"^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$")
+WS_PAT = re.compile(r"\s+")
 
 
-def translit(s):
-    return unicode(unidecode(unicode(s.encode("utf-8"), "utf-8")))
+def validate_age(field, value, error):
+    # Allow "NULL" string values, these will eventually represent `None`
+    # values.
+    if value != "NULL" and not AGE_PAT.match(value):
+        error(field, "must be an integer")
+    return
 
 
-def convert_isbn(s):
-    """
+def validate_not_null_str(field, value, error):
+    if value == "NULL":
+        error(field, "must not be 'NULL'")
 
-    ISBN values are 13 digits if assigned after Jan. 1, 2007, and 10 digits otherwise.
-    - last digit is a "check digit", which can be a letter or number.
 
-    """
+def validate_rating(field, value, error):
+    try:
+        rating = int(value)
+    except ValueError:
+        rating = -1
 
-    # Remove leading/trailing/inner whitespace.
-    # Make the raw ISBN upper case.
-    s = WS_PAT.sub("", s).upper()
-
-    if ISBN_PAT.match(s):
-        return unicode(s)
-
-    return ERROR_VALUE
+    if 0 <= rating <= 10:
+        error(field, "must be 0 <= x <= 10")
 
 
 def check_bx_users(fn):
+    """Check the consistentcy of the BX-Users.csv file.
+
     """
-    """
 
-    tab = ( etl.fromcsv(fn, delimiter=DELIMITER,
-                quoting=QUOTE_MINIMAL, encoding=ENCODING) )
+    tab = etl.fromcsv(fn, delimiter=DELIMITER,
+            quoting=QUOTE_ALL, encoding=ENCODING)
 
-    # Compute a summary of "bad" rows.
-    summary = defaultdict(int)
+    v = Validator({
+            "User-ID" : {
+                "type" : "string",
+                "regex" : USER_ID_PAT,
+                "required" : True
+            },
+            "Location" : {
+                "type" : "string",
+                "required" : True
+            },
+            "Age" : {
+                "type" : "string",
+                "validator" : validate_age,
+                "required" : True
+            }
+        })
 
-    for row_num, r in enumerate(tab.records(), 1):
-        # Check user-id format.
-        if r["User-ID"] == "NULL":
-            summary["User-ID, NULL"] += 1
-        elif not USER_ID_PAT.match(r["User-ID"]):
-            sys.stderr.write("WARNING: invalid `User-ID` format @ row %d"
-                    % row_num + os.linesep)
-            summary["User-ID, invalid"] += 1
-        else:
-            pass
-
-        # Check location format:
-        if r["Location"] == "NULL":
-            summary["Location, NULL"] += 1
-
-        # Check age format.
-        if r["Age"] == "NULL":
-            summary["(Age, NULL)"] += 1
-        elif not AGE_PAT.match(r["Age"]):
-            sys.stderr.write("WARNING: invalid `Age` format @ row %d"
-                    % row_num + os.linesep)
-            summary["(Age, invalid)"] += 1
-        else:
-            pass
-
-    # Print a summary of the bad rows.
-    print("SUMMARY for %s" % fn)
-    print(tabulate.tabulate(summary.items()))
+    for row_num, r in enumerate(tab.dicts(), 1):
+        is_valid = v.validate(r)
+        if not is_valid:
+            print("row %d -> %r, %r" % (row_num, v.errors, r))
 
     return
 
 
-def main():
-    conn = sqlite3.connect("bx.db")
+def check_bx_books(fn):
+    """Check the consistency of the BX-Books.csv file.
+
+    """
+
+    tab = etl.fromcsv(fn, delimiter=DELIMITER,
+            quoting=QUOTE_ALL, encoding=ENCODING)
+
+    v = Validator({
+            "ISBN" : {
+                "type" : "string",
+                "validator" : validate_not_null_str,
+                "required" : True,
+            },
+            "Book-Title" : {
+                "type" : "string",
+                "validator" : validate_not_null_str,
+                "required" : True
+            },
+            "Book-Author" : {
+                "type" : "string",
+                "validator" : validate_not_null_str,
+                "required" : True
+            },
+            "Year-Of-Publication" : {
+                "type" : "string",
+                "regex" : YEAR_PAT,
+                "required" : True
+            },
+            "Publisher" : {
+                "type" : "string",
+                "validator" : validate_not_null_str,
+                "required" : True
+            },
+            "Image-URL-S" : {
+                "type" : "string",
+                "regex" : URL_PAT,
+                "required" : True
+            },
+            "Image-URL-M" : {
+                "type" : "string",
+                "regex" : URL_PAT,
+                "required" : True
+            },
+            "Image-URL-L" : {
+                "type" : "string",
+                "regex" : URL_PAT,
+                "required" : True
+            }
+        })
+
+    for row_num, r in enumerate(tab.dicts(), 1):
+        is_valid = v.validate(r)
+        if not is_valid:
+            print("row %d -> %r, %r" % (row_num, v.errors, r))
+
+    return
+
+
+def check_bx_book_ratings(fn):
+    """Check the consistency of the BX-Book-Ratings.csv file.
+
+    """
+
+    tab = etl.fromcsv(fn, delimiter=DELIMITER,
+            quoting=QUOTE_ALL, encoding=ENCODING)
+
+    v = Validator({
+            "User-ID" : {
+                "type" : "string",
+                "required" : True
+            },
+            "ISBN" : {
+                "type" : "string",
+                "required" : True
+            },
+            "Book-Rating" : {
+                "type" : "string",
+                "validator" : validate_rating,
+                "required" : True
+            }
+        })
+
+    for row_num, r in enumerate(tab.dicts(), 1):
+        is_valid = v.validate(r)
+        if not is_valid:
+            print("row %d -> %r, %r" % (row_num, v.errors, r))
+
+    return
+
+
+def translit(s):
+    """Transliterate a non-ascii string to ascii.
+
+    """
+    return unicode(unidecode(unicode(s.encode("utf-8"), "utf-8")))
+
+
+def clean_isbn(s):
+    return WS_PAT.sub("", s)
+
+
+def clean_title(s):
+    title = s.strip()
+
+    if title == "NULL" or not title:
+        return NULL_VALUE
+    return title
+
+
+def clean_author(s):
+    author = s.strip()
+
+    if author == "NULL" or not author:
+        return NULL_VALUE
+    return author
+
+
+def clean_publisher(s):
+    publisher = s.strip()
+
+    if publisher == "NULL" or not publisher:
+        return NULL_VALUE
+    return publisher
+
+
+def clean_year(s):
+    year = s.strip()
+
+    try:
+        year = int(year)
+    except ValueError:
+        return NULL_VALUE
+
+    if year <= 0:
+        return NULL_VALUE
+    return year
+
+
+def clean_rating(s):
+    rating = s.strip()
+
+    try:
+        rating = int(rating)
+    except ValueError:
+        return NULL_VALUE
+
+    return rating
+
+
+def do_etl(bx_users_fn, bx_books_fn, bx_book_ratings_fn, db_fn):
+    """
+
+    Arguments:
+        bx_users_fn --
+        bx_books_fn --
+        bx_book_ratings_fn --
+        db_fn --
+
+    Returns:
+        ...
+
+    """
+
+    errargs = { "errorvalue" : ERROR_VALUE }
 
     #
     # Transform BX-Users.csv
     #
-
-    tab = etl.fromcsv("data/csv/BX-Users.csv", delimiter=DELIMITER,
-            quoting=QUOTE_MINIMAL, encoding=ENCODING)
+    tab = etl.fromcsv(bx_users_fn, delimiter=DELIMITER, escapechar=ESCAPECHAR,
+            quoting=QUOTE_ALL, encoding=ENCODING)
 
     tab = tab.rename({
         "User-ID" : "user_id",
@@ -123,15 +283,17 @@ def main():
         "Age" : "age"
         })
 
-    tab = ( tab.convert("user_id", translit, errorvalue=ERROR_VALUE)
-               .convert("location", translit, errorvalue=ERROR_VALUE)
-               .convert("age", lambda x : float(x), errorvalue=ERROR_VALUE) )
+    users_tab = (
+            tab.convert("user_id", (lambda s : s.strip()), **errargs)
+               .convert("location", translit, **errargs)
+               .convert("age", (lambda s : int(s)), **errargs) )
 
     #
     # Transform BX-Books.csv
     #
-    tab = etl.fromcsv("data/csv/BX-Books.csv", delimiter=DELIMITER,
-                quoting=QUOTE_MINIMAL, encoding=ENCODING)
+    tab = etl.fromcsv(bx_books_fn, delimiter=DELIMITER, escapechar=ESCAPECHAR,
+                quoting=QUOTE_ALL, encoding=ENCODING)
+
     tab = tab.rename({
                     "ISBN" : "isbn",
                     "Book-Title" : "title",
@@ -141,38 +303,59 @@ def main():
                     "Image-URL-S" : "img_url_s",
                     "Image-URL-M" : "img_url_m",
                     "Image-URL-L" : "img_url_l"
-                    })
-    tab = ( tab.convert("isbn", convert_isbn, errorvalue=ERROR_VALUE)
-            )
+                })
 
-    print tab.stringpatterns("isbn")
-
-    sys.exit()
-
-    #          .convert("isbn", translit, errorvalue=ERROR_VALUE)
-    #          .convert("title", translit, errorvalue=ERROR_VALUE)
-    #          .convert("author", lambda x : ERROR_VALUE if x == "NULL" translit, 
-
-
-    print tab.look(50)
-
-    sys.exit()
+    books_tab = (
+            tab.convert("isbn", clean_isbn, **errargs)
+               .convert("title", clean_title, **errargs)
+               .convert("author", clean_author, **errargs)
+               .convert("year", clean_year, **errargs)
+               .convert("publisher", clean_publisher, **errargs)
+               .convert("img_url_s", (lambda s : s.strip()), **errargs)
+               .convert("img_url_m", (lambda s : s.strip()), **errargs)
+               .convert("img_url_l", (lambda s : s.strip()), **errargs) )
 
     #
     # Transform BX-Book-Ratings.csv
     #
+    tab = etl.fromcsv(bx_book_ratings_fn, delimiter=DELIMITER,
+                escapechar=ESCAPECHAR, quoting=QUOTE_ALL, encoding=ENCODING)
+
+    tab = tab.rename({
+        "User-ID" : "user_id",
+        "ISBN" : "isbn",
+        "Book-Rating" : "rating"
+        })
+
+    ratings_tab = (
+            tab.convert("user_id", (lambda s : s.strip()), **errargs)
+               .convert("isbn", clean_isbn, **errargs)
+               .convert("rating", clean_rating, **errargs) )
 
     #
     # Load each table into an sqlite db.
     #
-    tab.todb(conn, "users", create=True)
-
-    conn.close()
+    with sqlite3.connect(db_fn) as conn:
+        users_tab.todb(conn, "users", create=True)
+        books_tab.todb(conn, "books", create=True, drop=True)
+        ratings_tab.todb(conn, "ratings", create=True)
+        conn.commit()
 
     return
+
 
 if __name__ == '__main__':
     opts = docopt(__doc__)
 
-    if opts["check"]:
+    if opts["etl"]:
+        do_etl(opts["<bx-users-csv>"], opts["<bx-books-csv>"],
+                opts["<bx-book-ratings-csv>"], opts["--db"])
+    elif opts["check"]:
+        print(opts["<bx-books-csv>"])
+        check_bx_books(opts["<bx-books-csv>"])
+
+        print(opts["<bx-users-csv>"])
         check_bx_users(opts["<bx-users-csv>"])
+
+        print(opts["<bx-book-ratings-csv>"])
+        check_bx_book_ratings(opts["<bx-book-ratings-csv>"])
